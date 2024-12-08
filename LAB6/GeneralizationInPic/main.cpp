b9e129b1b8face5dd83b85abab7442e9bcd00144
JanaHazem12 <jana.hazem.abbass@gmail.com>
2024-12-08 15:11:49 +0200

    LAB6 Submission

---
+++ LAB6/GeneralizationInPic/main.cpp
@@ -0,0 +1,238 @@
+// INCLUDE
+#include <iostream>
+#include <graphics.h>
+#include <winbgim.h>
+#include <windows.h>
+
+using namespace std;
+
+class Shape{
+    public:
+    virtual void draw()=0; // NO IMPLEMENTATION
+    int color;
+
+    Shape(){
+        color=RED;
+        cout<<"Non-parametrized Parent Constructor\n";
+    }
+    Shape(int color){
+        this->color=color;
+        cout<<"Parametrized Parent Constructor\n";
+
+    }
+    ~Shape(){
+        cout<<"PARENT DESTRUCTOR\n";
+    }
+};
+
+
+class Point{
+    private:
+    int x;
+    int y;
+    //Point point1;
+    //Point point2;
+
+    public:
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
+
+    Point():x(0),y(0){
+
+    }
+    Point(int a, int b):x(a),y(b){ // co-ords x,y
+
+    }
+    /*void draw(){
+        line(point1.getX(),point1.getY(),point2.getX(),point2.getY());
+    }*/
+};
+
+class Line:public Shape{ // MUST have a point (Line & Point --> COMPOSITION) + (Line & Picture --> AGGREGATION)
+    private:
+    Point point1;
+    Point point2;
+
+    public:
+    Line():point1(),point2(),Shape(){
+        cout<<"non-param LINE constructor"<<endl;
+    }
+    Line(int x1, int y1,int x2, int y2, int color):point1(x1,y1), point2(x2,y2),Shape(color){
+        cout<<"param. LINE constructor"<<endl;
+    }
+    ~Line(){
+        cout<<"LINE DESRTRUCTOR"<<endl;
+    }
+    void draw(){
+        line(point1.getX(),point1.getY(),point2.getX(),point2.getY());
+    }
+};
+
+class Rectanglee:public Shape{ // MUST have a LINE - (Rectangle & Point --> COMPOSITION) + (Rectangle & Picture --> AGGREGATION)
+    //Rectangle
+    private:
+    Point point1;
+    Point point2;
+
+    public:
+    Rectanglee(): point1(), point2(),Shape(){
+        cout<<"non-param. RECTANGLE constructor"<<endl;
+    }
+    Rectanglee(int x1, int y1, int x2, int y2, int color):point1(x1, y1),point2(x2, y2),Shape(color){
+        cout<<"param. RECTANGLE constructor"<<endl;
+    }
+    // 4 points BAS code 2 pts only (top left + bottom right)
+    ~Rectanglee(){
+        cout<<"RECTANGLE DESRTRUCTOR"<<endl;
+    }
+
+    void draw(){
+        rectangle(point1.getX(),point1.getY(),point2.getX(),point2.getY());
+    }
+};
+
+class Circle:public Shape{ // (Circle & Point --> COMPOSITION) + (Circle & Picture --> AGGREGATION)
+    private:
+    Point point1;
+    int radius;
+
+    public:
+    Circle():point1(),radius(0),Shape(){
+        cout<<"non-param. CIRCLE constructor"<<endl;
+
+    }
+    Circle(int x, int y, int r, int color):point1(x,y),radius(r),Shape(color){
+        cout<<"param. CIRCLE constructor"<<endl;
+
+    }
+    ~Circle(){
+        cout<<"CIRCLE DESTRUCTOR"<<endl;
+    }
+
+    void draw(){
+        circle(point1.getX(),point1.getY(),radius);
+    }
+};
+
+class Picture{
+    private:
+    int size;
+    int currentIndex=-1;
+    Shape ** ptr;
+
+    /* Line *L1;
+    Rectanglee *R1;
+    Circle *C1;
+
+    int LineCount;
+    int RectCount;
+    int CircleCount;
+*/
+    public:
+    Picture(int size){
+        this->size=size;
+        ptr=new Shape *[size];
+        currentIndex=-1;
+    }
+    /*
+    Picture(){
+        L1=NULL;
+        R1=NULL;
+        C1=NULL;
+        LineCount=0;
+        RectCount=0;
+        CircleCount=0;
+    }
+
+    void setCircle(int CC, Circle* circlee) {
+        CircleCount = CC;
+        C1 = circlee;
+    }
+
+    void setRectangle(int RC, Rectanglee*rectt) {
+        RectCount = RC;
+        R1 = rectt;
+    }
+
+    void setLine(int LC, Line* linee) {
+        LineCount = LC;
+        L1 = linee;
+    }*/
+
+    void paint() {
+        for(int i=0;i<=currentIndex; i++){
+                ptr[i]->draw();
+        }
+    }
+    void addShape(Shape *s){
+        if(currentIndex==size-1){
+            return;
+        }
+        currentIndex++;
+        ptr[currentIndex]=s;
+    }
+        /*for(int i=0; i<LineCount; i++){
+            L1[i].draw();
+        }
+        for(int i=0; i<RectCount; i++){
+            R1[i].draw();
+        }
+        for(int i=0; i<CircleCount; i++){
+            C1[i].draw();
+        }*/
+
+    /*void setLine(int, Line *);
+    void setRectangle(int Rectangle *);
+    void setCircle(int, Circle *);
+    void paint();*/
+};
+
+
+int main(){
+    /*Line L; // comp
+
+    Picture(&line);
+    Picture(&rect);
+    Picture(&cir);*/
+
+
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+    // Graphic Mode
+    //Picture myPic;
+    // 2nd & last --> up & down
+    // 1st & 3rd --> right & left
+    //Line ArrL[3] = {Line(190,70,220,70, RED),Line(190,195,220,195, RED),Line(190,320,220,320, RED)};
+    //Rectanglee ArrR[5] = {Rectanglee(220,30,300,125,RED),Rectanglee(220,150,300,245,RED),Rectanglee(220,270,300,365,RED),Rectanglee(10,150,150,245,RED),Rectanglee(600,300,150,275,RED)}; // 600,100 - 270,50
+    //Circle ArrC[3] = {Circle(50,50,50),Circle(200,100,100),Circle(420,50,30)};
+
+    // x1,x2,y1,y2
+
+    /*myPic.setLine(3, ArrL);
+    myPic.setRectangle(5, ArrR);
+    myPic.setCircle(3, ArrC);*/
+
+    Picture p(5); // 5 objects
+    p.addShape(new Line(16,17,18,19, RED));
+    p.addShape(new Circle(16,17,18, RED));
+    p.addShape(new Rectanglee(16,17,18,19, RED));
+    p.paint();
+
+    //myPic.paint();
+
+    //Shape s; // comment everything!!
+    /*Line L;
+    Rectanglee R;
+    Circle C;*/
+
+    getch();
+
+
+    return 0;
+}
+
+
---
+++ LAB6/GeoShape/main.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+class GeoShape{
+    protected:
+    float d1;
+    float d2;
+
+    public:
+    GeoShape(){
+        d1=0.0;
+        d2=0.0;
+    }
+    GeoShape(float dr){
+        d1=dr;
+    }
+    GeoShape(float d11, float d22){
+        d1=d11;
+        d2=d22;
+    }
+    ~GeoShape(){
+        cout<<"GEOSHAPE DESTRUCTOR"<<endl;
+    }
+    float getD1(){
+        return d1;
+    }
+    float getD2(){
+        return d2;
+    }
+    void setD1(float dd1){
+        d1 = dd1;
+    }
+    void setD2(float dd2){
+        d2 = dd2;
+    }
+
+};
+
+class Triangle:public GeoShape{
+    public:
+    Triangle():GeoShape(){
+    }
+    Triangle(float b, float h):GeoShape(b,h){ //  PARAMETRIZED CONSTRUCTOR
+        cout<<"PARAMETRIZED TRIANGLE CONSTRUCTOR"<<endl;
+    }
+    ~Triangle(){
+        cout<<"TRIANGLE DESTRUCTOR"<<endl;
+    }
+    float calculateAreaT(){ // no input params as I already took them in the constructor
+        float AreaT = 0.5 * d1 * d2;
+        cout<<AreaT<<endl;
+        return AreaT;
+    }
+
+};
+
+class Rectangle:public GeoShape{
+    public:
+    Rectangle():GeoShape(){
+    }
+    Rectangle(float x, float y):GeoShape(x,y){ //  PARAMETRIZED CONSTRUCTOR
+        cout<<"PARAMETRIZED RECTANGLE CONSTRUCTOR"<<endl;
+    }
+    ~Rectangle(){
+        cout<<"RECTANGLE DESTRUCTOR"<<endl;
+    }
+    float calculateAreaR(){ // no input params as I already took them in the constructor
+        float AreaR = d1 * d2;
+        cout<<AreaR<<endl;
+        return AreaR;
+    }
+
+};
+
+class Circle:private GeoShape{ // private in CIRCLE, but I can access it from PARENT 3ady
+    public:
+    Circle():GeoShape(){
+    }
+
+    Circle(float r):GeoShape(r){ //  PARAMETRIZED CONSTRUCTOR
+        cout<<"PARAMETRIZED CIRCLE CONSTRUCTOR"<<endl;
+    }
+    ~Circle(){
+        cout<<"CIRCLE DESTRUCTOR"<<endl;
+    }
+    /*void setRadius(float r1, float r2){
+        d1=r1;
+        d2=r2;
+    }*/
+    void setRadius(float r1){
+        d1=d2=r1;
+    }
+    float getRadius(){
+        return d1;
+    }
+
+    float calculateAreaC(){ // no input params as I already took them in the constructor
+        float AreaC = 3.141592653589793238462643383279502884197 * d1 * d2; // 3.14 * r^2
+        cout<<AreaC<<endl;
+        return AreaC;
+    }
+
+};
+
+class Square: private Rectangle{
+    /*Square(): Rectangle(){
+
+    }*/
+    public:
+    Square(float a): Rectangle(a,a){
+
+    }
+    float getSide(){
+        return d1;
+    }
+    void setSide(float x){
+        d1=d2=x;
+    }
+    float calculateAreaS(){
+        /*float AreaS = d1 * d2;
+        return AreaS;*/
+        return Rectangle::calculateAreaR(); // I can return d1*d2 bas keda NO OVERRIDING
+    }
+
+};
+
+int main()
+{
+    Triangle T(5,10);
+    T.calculateAreaT();
+    Rectangle R(3,2);
+    R.calculateAreaR();
+    Circle C(3);
+    C.calculateAreaC();
+    Square S(6);
+    S.calculateAreaS();
+    return 0;
+}
